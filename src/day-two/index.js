50e33e3d0826ac9b464435ea02d2d065c944b71a
Jordi Ayala <jord7as@gmail.com>
2022-12-02 16:15:54 -0600

    feat: :sparkles: day two

---
+++ src/day-two/index.js
@@ -0,0 +1,13 @@
+function countHours(year, holidays){
+  let hours = 0;
+  holidays.forEach(holiday => {
+    const date = new Date(`${holiday}/${year}`);
+    if (date.getDate() !== 31){
+      if(date.getDay() !== 0 && date.getDay() !== 6)
+        hours += 2;
+    }
+  });
+  return hours;
+}
+
+module.exports = countHours;
---
+++ test/day-two.test.js
@@ -0,0 +1,33 @@
+const countHours = require('../src/day-two/index.js');
+
+test("return type", () => {
+  const year = 2022;
+  const holidays = ['01/06', '04/01', '12/25'];
+  let hours = countHours(year, holidays);
+  expect(typeof hours).toBe('number');
+});
+
+test("countHours()", () => {
+  let hours = countHours(2023, ['01/06', '04/01', '12/25']);
+  expect(hours).toEqual(4);
+});
+
+test("countHours()", () => {
+  let hours = countHours(2022, ['01/06', '04/01', '12/25']);
+  expect(hours).toEqual(4);
+});
+
+test("countHours()", () => {
+  let hours = countHours(1985, ['01/01', '01/06', '02/02', '02/17', '02/28', '06/03', '12/06', '12/25']);
+  expect(hours).toEqual(10);
+});
+
+test("countHours()", () => {
+  let hours = countHours(2000, ['01/01']);
+  expect(hours).toEqual(0);
+});
+
+test("countHours()", () => {
+  let hours = countHours(2022, ['12/31']);
+  expect(hours).toEqual(0);
+});
