213ea95be4f344322cef4d093ae71cd06af85ccc
Jordi Ayala <jord7as@gmail.com>
2022-12-04 20:36:58 -0600

    perf: :zap: day three

--- src/day-three/index.js
+++ src/day-three/index.js
@@ -1,7 +1,5 @@
 function distributeGifts(packOfGifts, reindeers) {
-  const packWeight = packOfGifts.reduce((total, word) => total + word.length, 0);
-  const reindeersWeight = reindeers.reduce((total, word) => total + word.length * 2, 0);
-  return Math.floor(reindeersWeight / packWeight);
+  return Math.floor(reindeers.join('').length*2/packOfGifts.join('').length);
 }
 
 module.exports = distributeGifts;
