72eec16fb7ff362058a8372e6c177115e9349fb1
Jordi Ayala <jord7as@gmail.com>
2022-12-22 13:16:23 -0600

    refactor: :recycle: day twenty-two

--- src/day-twenty-two/index.js
+++ src/day-twenty-two/index.js
@@ -1,25 +1,7 @@
 function checkStepNumbers(systemNames, stepNumbers) {
-
-  let unionStepNumbers = systemNames.map((name, index) => ({
-    name: name,
-    stepNumber: stepNumbers[index]
-  }));
-
-  let groupStepNumbers = unionStepNumbers.reduce((acc, obj) => {
-    if (!acc[obj.name]) {
-      acc[obj.name] = [];
-    }
-    acc[obj.name].push(obj.stepNumber);
-    return acc;
-  }, {});
-  
-  const isOrdered = Object.values(groupStepNumbers).every((arr) => {
-    return arr.every(function(_, i) {
-      return i == 0 || arr[i] > arr[i - 1];
-    });
-  });
-
-  return isOrdered;      
+  return systemNames.every((el, i) => stepNumbers[i] <= stepNumbers[
+    i + systemNames.slice(i + 1).indexOf(el) + 1
+  ]);
 }
 
 module.exports = checkStepNumbers;
