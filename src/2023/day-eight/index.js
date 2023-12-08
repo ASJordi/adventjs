90ad6c86836dfce2f8a328e2d7a3624367bcd760
Jordi Ayala <jord7as@gmail.com>
2023-12-08 12:00:00 -0600

    feat(2023): :sparkles: day eight

---
+++ src/2023/day-eight/index.js
@@ -0,0 +1,21 @@
+function organizeGifts(gifts) {
+    const countGifts = gifts.match(/\d+/g).map(Number);
+    const nameGifts = gifts.match(/\D/g);
+
+    return countGifts.map((count, i) => {
+        const gift = nameGifts[i];
+        let result = '';
+
+        result += `[${gift}]`.repeat(count / 50);
+        count %= 50;
+
+        result += `{${gift}}`.repeat(count / 10);
+        count %= 10;
+
+        result += `(${gift.repeat(count)})`.repeat(+!!count);
+
+        return result;
+    }).join('');
+}
+
+module.exports = organizeGifts;
---
+++ test/2023/day-eight.test.js
@@ -0,0 +1,25 @@
+const organizeGifts = require('../../src/2023/day-eight/index.js')
+
+test('Test #01 - Return a String', () => {
+    expect(typeof organizeGifts(`76a11b`)).toBe('string');
+})
+
+test('Test #02 - organizeGifts("76a11b")', () => {
+    expect(organizeGifts("76a11b")).toStrictEqual("[a]{a}{a}(aaaaaa){b}(b)");
+})
+
+test('Test #03 - organizeGifts("20a")', () => {
+    expect(organizeGifts("20a")).toStrictEqual("{a}{a}")
+})
+
+test('Test #04 - organizeGifts("70b120a4c")', () => {
+    expect(organizeGifts("70b120a4c")).toStrictEqual("[b]{b}{b}[a][a]{a}{a}(cccc)")
+})
+
+test('Test #05 - organizeGifts("9c")', () => {
+    expect(organizeGifts("9c")).toStrictEqual("(ccccccccc)")
+})
+
+test('Test #06 - organizeGifts("19d51e")', () => {
+    expect(organizeGifts("19d51e")).toStrictEqual("{d}(ddddddddd)[e](e)")
+})
