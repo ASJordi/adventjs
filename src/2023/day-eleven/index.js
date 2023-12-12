8d55acca6e100da12afc943fa7e3c7ee33dd56ba
Jordi Ayala <jord7as@gmail.com>
2023-12-11 20:15:54 -0600

    feat(2023): :sparkles: day eleven

---
+++ src/2023/day-eleven/index.js
@@ -0,0 +1,28 @@
+function getIndexsForPalindrome(word) {
+    let res = null
+    for (const a of Array.from({ length: word.length }).keys()) {
+        for (const b of Array.from({ length: word.length }).keys()) {
+            let swapped = [...word]
+            let aux = word[a]
+            swapped[a] = word[b]
+            swapped[b] = aux
+
+            let left = swapped.slice(0, Math.floor(word.length / 2)).join("")
+            let right = swapped.slice(Math.ceil(word.length / 2)).reverse().join("")
+
+            res = [
+                [
+                    null
+                    , [
+                    []
+                    , [a, b]
+                ][+((a + b) > 0)]
+                ][+(left === right)]
+                , res
+            ][+!!res]
+        }
+    }
+    return res
+}
+
+module.exports = getIndexsForPalindrome;
---
+++ test/2023/day-eleven.test.js
@@ -0,0 +1,41 @@
+const getIndexsForPalindrome = require('../../src/2023/day-eleven/index.js');
+
+test('Test #01 - getIndexsForPalindrome(\'anna\')', () => {
+    expect(getIndexsForPalindrome('anna')).toStrictEqual([])
+})
+
+test('Test #02 - getIndexsForPalindrome(\'abab\')', () => {
+    expect(getIndexsForPalindrome('abab')).toStrictEqual([0, 1])
+})
+
+test('Test #03 - getIndexsForPalindrome(\'abac\')', () => {
+    expect(getIndexsForPalindrome('abac')).toStrictEqual(null)
+})
+
+test('Test #04 - getIndexsForPalindrome(\'aaaaaaaa\')', () => {
+    expect(getIndexsForPalindrome('aaaaaaaa')).toStrictEqual([])
+})
+
+test('Test #05 - getIndexsForPalindrome(\'aaababa\')', () => {
+    expect(getIndexsForPalindrome('aaababa')).toStrictEqual([1, 3])
+})
+
+test('Test #06 - getIndexsForPalindrome(\'caababa\')', () => {
+    expect(getIndexsForPalindrome('caababa')).toStrictEqual(null)
+})
+
+test('Test #07 - getIndexsForPalindrome(\'rotavator\')', () => {
+    expect(getIndexsForPalindrome('rotavator')).toStrictEqual([])
+})
+
+test('Test #08 - getIndexsForPalindrome(\'rotaratov\')', () => {
+    expect(getIndexsForPalindrome('rotaratov')).toStrictEqual([4, 8])
+})
+
+test('Test #09 - getIndexsForPalindrome(\'saippuakivikauppias\')', () => {
+    expect(getIndexsForPalindrome('saippuakivikauppias')).toStrictEqual([])
+})
+
+test('Test #10 - Return a function', () => {
+    expect(typeof getIndexsForPalindrome).toBe('function')
+})
