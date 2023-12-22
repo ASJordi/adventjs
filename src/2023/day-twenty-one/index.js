34171b9bb71bbdae4dae01452ce971006ec58b85
Jordi Ayala <jord7as@gmail.com>
2023-12-22 06:37:10 -0600

    feat(2023): :sparkles: day twenty-one

---
+++ src/2023/day-twenty-one/index.js
@@ -0,0 +1,27 @@
+function findBalancedSegment(message) {
+    let count = 0;
+    let start = 0;
+    let end = -1;
+    let map = new Map();
+    map.set(0, -1);
+
+    for (let i = 0; i < message.length; i++) {
+        count += message[i] === 1 ? 1 : -1;
+
+        if (map.has(count)) {
+            let segmentStart = map.get(count) + 1;
+            let segmentEnd = i;
+
+            if (segmentEnd - segmentStart > end - start) {
+                start = segmentStart;
+                end = segmentEnd;
+            }
+        } else {
+            map.set(count, i);
+        }
+    }
+
+    return end >= start ? [start, end] : [];
+}
+
+module.exports = findBalancedSegment;
---
+++ test/2023/day-twenty-one.test.js
@@ -0,0 +1,72 @@
+const findBalancedSegment = require('../../src/2023/day-twenty-one/index.js')
+
+test('Test #01 - Returns an array', () => {
+    expect(findBalancedSegment([1, 1, 0, 1, 1, 0, 1, 1]) instanceof Array).toBe(true)
+})
+
+test('Test #02', () => {
+    expect(findBalancedSegment([1, 1, 0, 1, 1, 0, 1, 1])).toStrictEqual(
+        [
+            2,
+            5
+        ]
+    )
+})
+
+test('Test #03', () => {
+    expect(findBalancedSegment([1, 1, 0])).toStrictEqual(
+        [
+            1,
+            2
+        ]
+    )
+})
+
+test('Test #04', () => {
+    expect(findBalancedSegment([1, 1, 1])).toStrictEqual([])
+})
+
+test('Test #05', () => {
+    expect(findBalancedSegment([1, 0, 1])).toStrictEqual(
+        [
+            0,
+            1
+        ]
+    )
+})
+
+test('Test #06', () => {
+    expect(findBalancedSegment([1, 0, 1, 0])).toStrictEqual(
+        [
+            0,
+            3
+        ]
+    )
+})
+
+test('Test #07', () => {
+    expect(findBalancedSegment([1, 1, 0, 1, 0, 1])).toStrictEqual(
+        [
+            1,
+            4
+        ]
+    )
+})
+
+test('Test #08', () => {
+    expect(findBalancedSegment([1, 0, 0, 0, 1, 1, 1, 0, 0, 0])).toStrictEqual(
+        [
+            0,
+            7
+        ]
+    )
+})
+
+test('Test #09', () => {
+    expect(findBalancedSegment([0, 1, 1, 1, 1, 0, 1, 1, 1, 0, 0, 1])).toStrictEqual(
+        [
+            5,
+            10
+        ]
+    )
+})
