a6a9396b07c7bde7e4a6091eb9c8dcb955191bc6
Jordi Ayala <jord7as@gmail.com>
2023-12-23 08:48:50 -0600

    feat(2023): :sparkles: day twenty-two

---
+++ src/2023/day-twenty-two/index.js
@@ -0,0 +1,34 @@
+function compile(code) {
+    let counter = 0;
+    let stack = -1;
+
+    for (let i = 0; i < code.length; i++) {
+        switch (code[i]) {
+            case '+':
+                counter++;
+                break;
+            case '-':
+                counter--;
+                break;
+            case '*':
+                counter *= 2;
+                break;
+            case '%':
+                stack = i;
+                break;
+            case '<':
+                if (stack !== -1) {
+                    i = stack;
+                    stack = -1;
+                }
+                break;
+            case '¿':
+                if (counter <= 0) i = code.indexOf('?', i);
+                break;
+        }
+    }
+
+    return counter;
+}
+
+module.exports = compile;
---
+++ test/2023/day-twenty-two.test.js
@@ -0,0 +1,49 @@
+const compile = require('../../src/2023/day-twenty-two/index.js')
+
+test('Test #01 - Returns a number', () => {
+    expect(typeof compile('++*-')).toBe('number')
+})
+
+test('Test #02', () => {
+    expect(compile('++*-')).toStrictEqual(3)
+})
+
+test('Test #03', () => {
+    expect(compile('++¿+?')).toStrictEqual(3)
+})
+
+test('Test #04', () => {
+    expect(compile('-+¿+?')).toStrictEqual(0)
+})
+
+test('Test #05', () => {
+    expect(compile('++*¿-?')).toStrictEqual(3)
+})
+
+test('Test #06', () => {
+    expect(compile('++%++<')).toStrictEqual(6)
+})
+
+test('Test #07', () => {
+    expect(compile('++%++<++¿*?')).toStrictEqual(16)
+})
+
+test('Test #08', () => {
+    expect(compile('++¿+?¿+?¿+?')).toStrictEqual(5)
+})
+
+test('Test #09', () => {
+    expect(compile('--¿+++?')).toStrictEqual(-2)
+})
+
+test('Test #10', () => {
+    expect(compile('--¿+++?+++¿--?')).toStrictEqual(-1)
+})
+
+test('Test #11', () => {
+    expect(compile('<%+¿++%++<?')).toStrictEqual(7)
+})
+
+test('Test #12', () => {
+    expect(compile('<<<<<<+<<<<<+%')).toStrictEqual(2)
+})
